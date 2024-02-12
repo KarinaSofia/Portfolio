bbb8a2b997ca34b8532d9e5ef16e951c969a6966
Karina Evangelista <karinaevang@hotmail.com>
2024-02-12 14:32:39 -0500

    deployemnt

--- portfolio_fe/src/app/page.tsx
+++ portfolio_fe/src/app/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import styles from './Home.module.css';
 import pfImage from '../../public/pfImage.jpg';
 import Navigationbar from "@/app/components/navigationbar";
-import HomeFooter from "@/app/components/homeFooter";
 import React from "react";
 
 export default function Home() {
@@ -17,11 +16,17 @@ export default function Home() {
                     <div className="w-1/2 p-4 my-6 justify-center">
                         <h1>Karina Evangelista</h1>
                         <h2 className={`${styles.subtitle} my-4`}>Hello~ I’m <span className={styles.highlight}>Karina</span>, I’m a computer science student studying at Champlain College</h2>
-                        <a href="/masterCV.pdf" download="masterCV.pdf">
+                        <a href="/masterCV.pdf" download="masterCV.pdf" className="mb-4">
                             <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 w-40 rounded hover:-translate-y-1 transition-transform duration-300">
                                 Download CV
                             </button>
                         </a>
+                        <a href="https://www.linkedin.com/in/karina-evangelista" className="ml-4">
+                            <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 w-40 rounded hover:-translate-y-1 transition-transform duration-300">
+                                Go To LinkedIn
+                            </button>
+                        </a>
+
 
                     </div>
 
