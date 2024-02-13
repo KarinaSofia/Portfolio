d54a48527626da324a0e938e0c86fb4d3cc31a2f
Karina Evangelista <karinaevang@hotmail.com>
2024-02-13 17:06:02 -0500

    pls

--- portfolio_backend/src/main/java/com/karina/portfolio_backend/EmailService/EmailController.java
+++ portfolio_backend/src/main/java/com/karina/portfolio_backend/EmailService/EmailController.java
@@ -16,6 +16,7 @@ public class EmailController {
         this.emailService = emailService;
     }
 
+    @CrossOrigin(origins = "https://portfolio-ten-rose-31.vercel.app")
     @PostMapping("/send-email")
     public void sendEmail(@RequestBody EmailRequest emailRequest) throws MessagingException {
         emailService.sendEmailToKarina(emailRequest);
